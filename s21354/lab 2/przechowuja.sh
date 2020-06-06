db0563b87ab70740073c58092e84029305ab4107
Tavi <56189672+s21354-pj@users.noreply.github.com>
2020-06-06 10:24:54 +0200

    Update przechowuja.sh

--- s21354/lab 2/przechowuja.sh
+++ s21354/lab 2/przechowuja.sh
@@ -3,7 +3,7 @@
 
 echo $PATH     #lokalizacja
 echo $RANDOM   #pseudolosowe liczby
-echo $PWD      #umiejscowienie skryptu
+echo $PWD      #aktualny katalog roboczy z momentu uruchomienia skryptu
 echo $PS1      #zmienia znak zachety
 echo $USER     #nazwa uzytkownika
 echo $HOSTNAME #nazwa servera
