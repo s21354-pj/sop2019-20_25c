4193f71ff2112826dc47b8fa34bb2eaf45d0b71e
Tavi <56189672+s21354-pj@users.noreply.github.com>
2020-06-14 10:40:39 +0200

    Delete zad2-2.sh

--- s21354/zajecia2/zad2-2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-A="Tekst 1"
-B="tekst:$A" # " wypisuje to, co mu sie zada uwzgledniajac podane dane
-echo "$B"
-B='tekst:$A' #pojedynczy ' wypisuje dokladnie to, co mu sie zada'
-echo "$B"
-B=tekst:$A
-echo "$B"
